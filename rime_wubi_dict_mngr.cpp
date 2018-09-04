1e259f25dbeabd696856ea76f0d246ffe113a4c6
Krist Pan <panqiincs@gmail.com>
2018-09-04 15:56:54 +0800

    initial commit

---
+++ rime_wubi_dict_mngr.cpp
@@ -0,0 +1,6 @@
+#include "rime_wubi_dict_mngr.h"
+
+RimeWubiDictMngr::RimeWubiDictMngr()
+{
+
+}
---
+++ rime_wubi_dict_mngr.h
@@ -0,0 +1,11 @@
+#ifndef RIME_WUBI_DICT_MNGR_H
+#define RIME_WUBI_DICT_MNGR_H
+
+
+class RimeWubiDictMngr
+{
+public:
+    RimeWubiDictMngr();
+};
+
+#endif // RIME_WUBI_DICT_MNGR_H
